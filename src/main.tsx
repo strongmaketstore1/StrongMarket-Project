0731567322e2f671110f6c3f9313e6af8fecc436
strongmaketstore1 <zagganations@gmail.com>
2026-08-26 12:50:27 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,20 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
+const redirect = sessionStorage.getItem("redirect");
+
+if (redirect) {
+  sessionStorage.removeItem("redirect");
+
+  if (window.location.pathname === "/StrongMarket-Project/") {
+    window.history.replaceState(
+      null,
+      "",
+      redirect
+    );
+  }
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
