67c0c2811d96a2a3b57fceb7fbce200a6e791a57
strongmaketstore1 <zagganations@gmail.com>
2026-09-11 05:16:11 +0000

    Fix product links

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -31,7 +31,7 @@ export default function ProductCard({ product }: ProductCardProps) {
             </span>
           </div>
 
-          <Link to={`/product/₦{product.slug}`}>
+          <Link to={`/product/${product.slug}`}>
   View Product
 </Link>
         </div>
