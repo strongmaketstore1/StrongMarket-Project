d6e019f778a6f6d2a97016612d0e30872232b673
strongmaketstore1 <zagganations@gmail.com>
2026-09-25 19:30:46 +0100

    Update MerchantDashboard.tsx

--- src/pages/MerchantDashboard.tsx
+++ src/pages/MerchantDashboard.tsx
@@ -1,12 +1,8 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import {
-  collection,
   doc,
   getDoc,
-  getDocs,
-  query,
-  where,
 } from "firebase/firestore";
 import {
   onAuthStateChanged,
